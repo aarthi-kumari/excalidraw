da6ad8477d4ecb5ed81ab050bd5f58955dc8c144
aarthi-kumari <aarthik584@gmail.com>
2026-03-20 14:14:58 +0530

    added signup/signin

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,42 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
 const app = express();
 
-app.listen(3000);
+app.post("/signup",(req, res) =>{
+	res.json({
+		userId: "123"
+	})
+
+} )
+
+app.post("/signin",(req, res) =>{
+     const userId = 1;
+	 const token = jwt.sign({
+		userId
+	 }, JWT_SECRET);
+	 res.json({
+		token
+   })
+})
+
+app.post("/room", middleware,(req, res) => {
+	res.json({
+		roomId: 123
+	})
+ 
+})
+app.listen(3001);
+
+
+
+
+
+
+const port = Number(process.env.PORT ?? 3001);
+
+app.listen(port, () => {
+	console.log(`http-backend listening on http://localhost:${port}`);
+});
